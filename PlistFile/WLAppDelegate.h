c029fb764761e436f40de3a7d5955a2698b18ea5
Mharis.Khalid <informharis@gmail.com>
2012-02-03 20:35:39 +0800

    Initial Commit

---
+++ PlistFile/WLAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  WLAppDelegate.h
+//  PlistFile
+//
+//  Created by hazwan abu hassan on 2/3/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class WLViewController;
+
+@interface WLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) WLViewController *viewController;
+
+@end
---
+++ PlistFile/WLViewController.h
@@ -0,0 +1,13 @@
+//
+//  WLViewController.h
+//  PlistFile
+//
+//  Created by hazwan abu hassan on 2/3/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WLViewController : UIViewController
+
+@end
